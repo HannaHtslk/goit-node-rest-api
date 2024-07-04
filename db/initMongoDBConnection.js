51eeb70ab52c09b8e9935cfc0cf7d8d0d9dbf2d9
AnnaHtslk <hannah.htslk@gmail.com>
2024-07-04 14:41:15 +0300

    hid credentials

--- db/initMongoDBConnection.js
+++ db/initMongoDBConnection.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
+import { DB_HOST } from "./config";
 
 const initMongoDBConnection = async () => {
   try {
-    const DB_HOST =
-      "mongodb+srv://hannahhtslk:LjXWqAjp4eUajT48@cluster0.wxj0ri1.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0";
-
     await mongoose.connect(DB_HOST);
 
     console.log("Database connection successful");
