e67724086d267125b14152dfd8ae3d16a622ee8f
AnnaHtslk <hannah.htslk@gmail.com>
2024-07-11 17:19:25 +0300

    added signin function

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -27,3 +27,32 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await findUser({ email });
+
+    if (!user) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const passwordVerification = await bcrypt.compare(password, user.password);
+    if (!passwordVerification) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const token = "1213.3431.23323";
+
+    res.json({
+      status: 200,
+      token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import validateBody from "../helpers/validateBody.js";
 import { userAuthSchema } from "../schemas/authSchemas.js";
-import { signup } from "../controllers/userControllers.js";
+import { signin, signup } from "../controllers/userControllers.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", validateBody(userAuthSchema), signup);
+
+authRouter.post("/login", validateBody(userAuthSchema), signin);
 
 export default authRouter;
