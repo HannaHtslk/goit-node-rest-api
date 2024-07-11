ca6499b1c15a2d8e5584368fa17a14013e84a6dc
AnnaHtslk <hannah.htslk@gmail.com>
2024-07-11 16:43:32 +0300

    hashed password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,16 +1,20 @@
 import HttpError from "../helpers/HttpError.js";
 import { findUser, userSignup } from "../services/userServices.js";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const user = await findUser({ email });
 
     if (user) {
       throw HttpError(409, "Email in use");
     }
 
-    const newUser = await userSignup(req.body);
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const newUser = await userSignup({ ...req.body, password: hashedPass });
+
     res.status(201).json({
       status: 201,
       message: "Created",
